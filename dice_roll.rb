68feb70207a9120f67d95a473c9a6b68751a1100
studioshane <studioshane@gmail.com>
2018-12-17 14:58:32 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,11 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+
+  allamaraine = [1,2,3,4,5,6]
+
+  i = rand(0..5)
+  result = allamaraine[i]
+
+  return result
 end
